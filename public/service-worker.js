c9ab6170dca79c6b4b2af3bb30840c9196e73c50
Nick Johnson <nickcjohnson84@outlook.com>
2022-05-12 22:41:03 -0600

    starting service worker

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/assets/css/bootstrap.min.css',
+  '/assets/css/style.css',
+  '/assets/icons/icon-72x72.png',
+  '/assets/icons/icon-96x96.png',
+  '/assets/icons/icon-128x128.png',
+  '/assets/icons/icon-144x144.png',
+  '/assets/icons/icon-152x152.png',
+  '/assets/icons/icon-192x192.png',
+  '/assets/icons/icon-384x384.png',
+  '/assets/icons/icon-512x512.png',
+  '/assets/js/index.js',
+  '/assets/js/indexedDB.js',
+];
+
+// Install Service Worker
